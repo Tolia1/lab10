4c15b94116f408a1d9f19d7f9ed026c28a606464
Anatolii Smirnov <anatoly31122002@gmail.com>
2024-04-16 21:57:52 +0300

    Добавьте файлы проекта.

---
+++ lab11-2/App_Filters/LoggingFilterAttribute.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace lab11_2.App_Filters
+{
+    public class LoggingFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            // Запис імені методу дії та часу виконання
+            var controllerDescriptor = context.ActionDescriptor as ControllerActionDescriptor;
+            if (controllerDescriptor != null)
+            {
+                string logEntry = $"Action Method: {controllerDescriptor.ActionName} | Execution Time: {DateTime.Now}";
+                File.AppendAllText("log.txt", logEntry + Environment.NewLine);
+            }
+
+            base.OnActionExecuted(context);
+        }
+    }
+}
---
+++ lab11-2/App_Filters/UserCountFilterAttribute.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace lab11_2.App_Filters
+{
+    public class UserCountFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            string userIP = null;
+            var httpContext = context.HttpContext;
+
+            if (httpContext != null)
+            {
+                var ipAddress = httpContext.Connection.RemoteIpAddress;
+                if (ipAddress != null)
+                {
+                    userIP = ipAddress.ToString();
+                }
+            }
+
+            using (StreamReader reader = new StreamReader("users.txt"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line == userIP)
+                    {
+                        return;
+                    }
+                }
+            }
+
+            // Запис IP-адреси користувача
+            File.AppendAllText("users.txt", userIP + Environment.NewLine);
+
+            base.OnActionExecuting(context);
+        }
+    }
+
+}
